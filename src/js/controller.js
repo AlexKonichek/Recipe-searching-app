5b15a9555be3f34f276718529ff0d743db800850
Alex Konichek <Konichekalex@gmail.com>
2020-12-04 15:30:22 +0200

    added loader when recipes data is fetching

--- src/js/controller.js
+++ src/js/controller.js
@@ -13,10 +13,19 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-
+const renderSpiner = function (parentEl) {
+  const markup = ` <<div class="spinner">
+  <svg>
+    <use href="${icons}#icon-loader"></use>
+  </svg>
+</div>`
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+}
 const showRecipe = async function (params) {
   try {
     //loading recipes
+  renderSpiner(recipeContainer);
   const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`);
   const data = await res.json();
   if(!res.ok){
